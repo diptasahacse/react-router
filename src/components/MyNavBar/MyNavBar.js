43b5eaecc0e637c612388a8dc1de1d973ab7ef3a
diptasahacse <diptasaha.lpu.cse@gmail.com>
2022-03-30 17:03:34 +0600

    make nav like working without reload and set custom active link

--- src/components/MyNavBar/MyNavBar.js
+++ src/components/MyNavBar/MyNavBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
 
 const MyNavBar = () => {
     return (
@@ -9,11 +11,22 @@ const MyNavBar = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
+                        {/* <Nav.Link>Home</Nav.Link>
                         <Nav.Link href="/members">Members</Nav.Link>
                         <Nav.Link href="/blogs">Blogs</Nav.Link>
                         <Nav.Link href="/about">About</Nav.Link>
-                        <Nav.Link href="/contact">Contact Us</Nav.Link>
+                        <Nav.Link href="/contact">Contact Us</Nav.Link> */}
+                        <CustomLink  className='nav-link' to="/">Home</CustomLink>
+                        <CustomLink  className='nav-link' to="/members">Members</CustomLink>
+                        <CustomLink  className='nav-link' to="/blogs">Blogs</CustomLink>
+                        <CustomLink  className='nav-link' to="/about">About</CustomLink>
+                        <CustomLink  className='nav-link' to="/contact">Contact Us</CustomLink>
+                        
+                        {/* <Link className='nav-link' to="/members">Members</Link>
+                        <Link className='nav-link' to="/blogs">Blogs</Link>
+                        <Link className='nav-link' to="/about">About</Link>
+                        <Link className='nav-link' to="/contact">Contact Us</Link> */}
+                       
                         
                     </Nav>
                 </Navbar.Collapse>
